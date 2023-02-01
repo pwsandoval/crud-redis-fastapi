aafef2e0957db6405c87aef11b7bd98ead1612e3
Peter <pwsandoval@outlook.com>
2023-02-01 15:17:54 -0500

    expose port of redis from docker

--- redis-crud/db/connection.py
+++ redis-crud/db/connection.py
@@ -5,7 +5,7 @@ from redis.exceptions import ConnectionError
 try:
     client = Redis(
         host="127.0.0.1",
-        port="61413",
+        port="6379",
         password="master_password",
         charset="utf-8",
         decode_responses=True,
